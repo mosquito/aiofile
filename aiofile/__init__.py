adaf9024a226511bdd6166988f3a3fd3d80b7332
Dmitry Orlov <me@mosquito.su>
2017-01-04 01:42:02 +0300

    [init]

--- aiofile/__init__.py
+++ aiofile/__init__.py
@@ -1,19 +1,9 @@
-__all__ = (
-    '__author__',
-    '__version__',
-    'author_info',
-    'license',
-    'package_info',
-    'version_info',
-    'AIOFile',
-    'Reader',
-    'Writer',
-)
-
+from .utils import Reader, Writer
 
 try:
     from ._aio import AIOFile as _AIOFile
 except ImportError:
     from ._thread import AIOFile as _AIOFile
 
-from .utils import Reader, Writer
+
+__all__ = 'AIOFile', 'Reader', 'Writer'
