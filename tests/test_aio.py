a29fa7fd3b1fcf94ef0769634dde78f492adcbd3
Dmitry Orlov <me@mosquito.su>
2017-06-26 16:23:15 +0300

    [fix] seek fixes

--- tests/test_aio.py
+++ tests/test_aio.py
@@ -1,4 +1,5 @@
 import sys
+import asyncio
 from aiofile import AIOFile
 from aiofile.utils import Reader, Writer
 from aiofile.posix_aio import AIOOperation, IO_WRITE, IO_NOP, IO_READ
@@ -107,4 +108,27 @@ def test_reader_writer(temp_file, uuid):
     assert count == 100
 
 
-# TODO: Add tests with changing offset
+@pytest.mark.asyncio
+def test_parallel_writer(temp_file, uuid):
+    w_file = posix_aio_file(temp_file, 'w')
+    r_file = posix_aio_file(temp_file, 'r')
+
+    futures = list()
+
+    for i in range(2000):
+        futures.append(w_file.write(uuid, i * len(uuid), 0))
+
+    yield from asyncio.wait(futures)
+    yield from w_file.fsync()
+
+    count = 0
+    for async_chunk in Reader(r_file, chunk_size=len(uuid)):
+        chunk = yield from async_chunk
+
+        if not chunk:
+            break
+
+        assert chunk.decode() == uuid
+        count += 1
+
+    assert count == 2000
