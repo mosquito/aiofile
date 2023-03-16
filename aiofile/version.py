0ff87df338bf6eee51fb6d04cdc7d320144e1c64
Dmitry Orlov <me@mosquito.su>
2023-03-16 12:01:01 +0300

    bump to 3.8.5

--- aiofile/version.py
+++ aiofile/version.py
@@ -9,7 +9,7 @@ package_license = "Apache 2"
 
 team_email = "me@mosquito.su"
 
-version_info = (3, 8, 4)
+version_info = (3, 8, 5)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
