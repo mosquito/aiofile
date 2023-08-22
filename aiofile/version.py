e47ef1653ebe03017f88e89efcf2f13c1c52e64d
Dmitry Orlov <me@mosquito.su>
2023-08-22 19:06:20 +0300

    bump major version

--- aiofile/version.py
+++ aiofile/version.py
@@ -9,7 +9,7 @@ package_license = "Apache 2"
 
 team_email = "me@mosquito.su"
 
-version_info = (3, 9, 0)
+version_info = (4, 0, 0)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
