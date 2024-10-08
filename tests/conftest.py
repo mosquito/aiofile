6eee29ed754f633171a5d98ea54b2dddb82c6bcb
Dmitry Orlov <me@mosquito.su>
2024-10-08 12:19:33 +0200

    Reformat and fix types

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from functools import partial
+from types import ModuleType
+from typing import List, Union
 
 import pytest
 from caio import python_aio_asyncio
@@ -9,35 +11,37 @@ from aiofile import AIOFile, async_open
 try:
     from caio import thread_aio_asyncio
 except ImportError:
-    thread_aio_asyncio = None
+    thread_aio_asyncio = None       # type: ignore
 
 try:
     from caio import linux_aio_asyncio
 except ImportError:
-    linux_aio_asyncio = None
+    linux_aio_asyncio = None        # type: ignore
 
 
-class DefaultContext:
-    __name__ = "default"
-
-
-IMPLEMENTATIONS = list(
+IMPLEMENTATIONS: List[Union[ModuleType, None]] = list(
     filter(
         None, [
             linux_aio_asyncio,
             thread_aio_asyncio,
             python_aio_asyncio,
-            DefaultContext(),
         ],
     ),
 )
 
-IMPLEMENTATION_NAMES = map(lambda x: x.__name__, IMPLEMENTATIONS)
+IMPLEMENTATIONS.insert(0, None)
+
+IMPLEMENTATION_NAMES: List[str] = list(
+    map(
+        lambda x: x.__name__ if x is not None else "default",
+        IMPLEMENTATIONS
+    )
+)
 
 
 @pytest.fixture(params=IMPLEMENTATIONS, ids=IMPLEMENTATION_NAMES)
 async def aio_context(request, event_loop):
-    if isinstance(request.param, DefaultContext):
+    if request.param is None:
         yield None
         return
 
